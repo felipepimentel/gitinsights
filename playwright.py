4128c5bba0efa8189a2af5322bbc1058da7403a7
felipepimentel <fpimentel88@gmail.com>
2023-11-25 15:49:57 -0300

    asdasd

--- playwright.py
+++ playwright.py
@@ -6,35 +6,36 @@ services:
     ports:
       - "3000:3000"
     environment:
-      # Defina as variáveis de ambiente para a configuração do Huginn
-      DATABASE_ADAPTER: mysql2
-      DATABASE_ENCODING: utf8mb4
-      DATABASE_RECONNECT: 'true'
-      DATABASE_NAME: huginn
-      DATABASE_POOL: 10
-      DATABASE_USERNAME: root
-      DATABASE_PASSWORD: ${MYSQL_ROOT_PASSWORD}
-      DATABASE_HOST: mysql
-      # Variáveis para o SMTP (se quiser enviar e-mails)
-      SMTP_DOMAIN: example.com
-      SMTP_USER_NAME: ${SMTP_USER_NAME}
-      SMTP_PASSWORD: ${SMTP_PASSWORD}
-      SMTP_SERVER: smtp.example.com
-      SMTP_PORT: 587
-      SMTP_AUTHENTICATION: 'login'
-      SMTP_ENABLE_STARTTLS_AUTO: 'true'
-      EMAIL_FROM_ADDRESS: noreply@example.com
-    depends_on:
-      - mysql
+      # Configurações do Huginn (igual ao exemplo anterior)
+      # ...
 
-  mysql:
+  mysql-huginn:
     image: mysql:5.7
     environment:
-      MYSQL_ROOT_PASSWORD: my-secret-pw # Defina uma senha forte aqui
+      MYSQL_ROOT_PASSWORD: my-secret-pw-huginn
       MYSQL_DATABASE: huginn
     volumes:
-      - mysql-data:/var/lib/mysql
+      - mysql-data-huginn:/var/lib/mysql
+
+  freshrss:
+    image: freshrss/freshrss
+    ports:
+      - "8080:80"
+    depends_on:
+      - mysql-freshrss
+    volumes:
+      - freshrss-data:/var/www/FreshRSS/data
+
+  mysql-freshrss:
+    image: mysql:5.7
+    environment:
+      MYSQL_ROOT_PASSWORD: my-secret-pw-freshrss
+      MYSQL_DATABASE: freshrss
+    volumes:
+      - mysql-data-freshrss:/var/lib/mysql
 
 volumes:
-  mysql-data:
+  mysql-data-huginn:
+  mysql-data-freshrss:
+  freshrss-data:
 
