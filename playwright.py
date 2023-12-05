bdcfc97d47dae6496434d06717536ed040c2057d
felipepimentel <fpimentel88@gmail.com>
2023-12-05 11:10:00 -0300

    sss

--- playwright.py
+++ playwright.py
@@ -1,41 +1,39 @@
-version: '3'
+import pandas as pd
+from bs4 import BeautifulSoup
 
-services:
-  huginn:
-    image: huginn/huginn
-    ports:
-      - "3000:3000"
-    environment:
-      # Configurações do Huginn (igual ao exemplo anterior)
-      # ...
+def html_form_to_data_frame(content, form_id):
+    soup = BeautifulSoup(content, "html.parser")
+    form = soup.find('form', id=form_id)
+    if not form:
+        raise ValueError(f"Form with id {form_id} not found")
+    
+    data = []
+    # Extrai todos os elementos de input, textarea e select
+    for element in form.find_all(['input', 'textarea', 'select']):
+        element_id = element.get('id')
+        label = soup.find('label', {'for': element_id})
+        label_text = label.get_text(strip=True) if label else ""
+        
+        # Trata diferentes tipos de elementos de entrada
+        if element.name == 'input':
+            input_type = element.get('type', 'text')
+            if input_type in ['checkbox', 'radio']:
+                value = element.get('checked')
+            else:
+                value = element.get('value')
+        elif element.name == 'textarea':
+            value = element.text
+        elif element.name == 'select':
+            selected_option = element.find('option', selected=True)
+            value = selected_option.get('value') if selected_option else None
+        
+        data.append({"label": label_text, "value": value})
+    
+    df = pd.DataFrame(data)
+    return df
 
-  mysql-huginn:
-    image: mysql:5.7
-    environment:
-      MYSQL_ROOT_PASSWORD: my-secret-pw-huginn
-      MYSQL_DATABASE: huginn
-    volumes:
-      - mysql-data-huginn:/var/lib/mysql
-
-  freshrss:
-    image: freshrss/freshrss
-    ports:
-      - "8080:80"
-    depends_on:
-      - mysql-freshrss
-    volumes:
-      - freshrss-data:/var/www/FreshRSS/data
-
-  mysql-freshrss:
-    image: mysql:5.7
-    environment:
-      MYSQL_ROOT_PASSWORD: my-secret-pw-freshrss
-      MYSQL_DATABASE: freshrss
-    volumes:
-      - mysql-data-freshrss:/var/lib/mysql
-
-volumes:
-  mysql-data-huginn:
-  mysql-data-freshrss:
-  freshrss-data:
-
+# Exemplo de uso:
+# content = seu_html_aqui
+# form_id = 'seu_form_id_aqui'
+# df = html_form_to_data_frame(content, form_id)
+# print(df)
