972b9b50f4e7522366815219b900d1045141ac92
felipepimentel <fpimentel88@gmail.com>
2023-12-10 12:26:18 -0300

    asas

--- playwright.py
+++ playwright.py
@@ -1,10 +1,11 @@
 import asyncio
 from playwright.async_api import async_playwright
 import pandas as pd
+import json
 
 async def extract_form_data(page, form_selector):
     form_data = []
-    
+
     # Selecione todos os elementos de entrada dentro do formulário
     input_elements = await page.query_selector_all(f"{form_selector} input, {form_selector} textarea, {form_selector} select")
     
@@ -16,21 +17,25 @@ async def extract_form_data(page, form_selector):
         label_text = await label.inner_text() if label else name_or_id  # Usa o ID ou o nome como backup se a label não existir
         
         if element_tag_name == 'SELECT':
-            value = await page.evaluate('(element) => element.options[element.selectedIndex].text', element)
-        elif element_tag_name in ['INPUT', 'TEXTAREA']:
+            value = await page.evaluate('(element) => element.options[element.selectedIndex].value', element)
+        elif element_tag_name == 'INPUT':
             type_attr = await page.evaluate('(element) => element.type', element)
             if type_attr == 'checkbox' or type_attr == 'radio':
-                is_checked = await page.evaluate('(element) => element.checked', element)
-                value = 'checked' if is_checked else 'unchecked'
+                value = await page.evaluate('(element) => element.checked', element)
             else:
-                value = await page.input_value(selector=f"#{name_or_id}")
+                value = await page.evaluate('(element) => element.value', element)
+        elif element_tag_name == 'TEXTAREA':
+            value = await page.evaluate('(element) => element.value', element)
         else:
             continue  # Pule elementos que não são reconhecidos
         
         form_data.append({"label": label_text, "value": value})
     
-    # Converta a lista de dicionários para um DataFrame
-    df = pd.DataFrame(form_data)
+    # Converta a lista de dicionários para uma string JSON
+    json_content = json.dumps(form_data)
+    
+    # Crie um DataFrame com a coluna content contendo a string JSON
+    df = pd.DataFrame([{"content": json_content}])
     return df
 
 async def main():
@@ -43,7 +48,7 @@ async def main():
         # Substitua 'seu_seletor_de_formulário' pelo seletor CSS do seu formulário
         data_frame = await extract_form_data(page, 'seu_seletor_de_formulário')
         
-        print(data_frame)
+        print(data_frame.to_string(index=False))  # Mostra o DataFrame sem o índice
 
         await browser.close()
 
