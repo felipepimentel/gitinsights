f5b4f1e2f6d913474040d3d3ba27ceabb2ac1149
felipepimentel <fpimentel88@gmail.com>
2023-12-10 11:29:06 -0300

    asas

--- playwright.py
+++ playwright.py
@@ -1,58 +1,64 @@
-import re
+import asyncio
+from playwright.async_api import async_playwright
+import pandas as pd
 
-class YamlProcessor:
-    def __init__(self, var_dict):
-        self.variable_pattern = re.compile(r'\$\((\w+)\)')
-        self.var_dict = var_dict
+async def extract_form_data(page, form_selector):
+    # Esperar pelo carregamento completo da página
+    await page.wait_for_load_state('networkidle')
 
-    def update_line(self, key):
-        # Get the original value from the dictionary
-        original_value = self.var_dict.get(key, "")
-        # If it's not a string, return as is
-        if not isinstance(original_value, str):
-            return original_value
+    # Obter todos os elementos de entrada do formulário
+    input_elements = await page.query_selector_all(f'{form_selector} input')
+    select_elements = await page.query_selector_all(f'{form_selector} select')
+    textarea_elements = await page.query_selector_all(f'{form_selector} textarea')
+
+    # Dicionário para armazenar os dados do formulário
+    form_data = {}
+
+    # Extrair dados dos inputs
+    for element in input_elements:
+        element_type = await element.get_attribute('type')
+        element_name = await element.get_attribute('name') or await element.get_attribute('id')
+        if element_type == 'checkbox' or element_type == 'radio':
+            value = await element.is_checked()
+        else:
+            value = await element.input_value()
+        form_data[element_name] = value
+
+    # Extrair dados dos selects
+    for element in select_elements:
+        element_name = await element.get_attribute('name') or await element.get_attribute('id')
+        value = await element.input_value()
+        form_data[element_name] = value
+
+    # Extrair dados dos textareas
+    for element in textarea_elements:
+        element_name = await element.get_attribute('name') or await element.get_attribute('id')
+        value = await element.input_value()
+        form_data[element_name] = value
+
+    # Converter para DataFrame
+    df = pd.DataFrame([form_data])
+
+    return df
+
+async def main():
+    # Inicializar o Playwright e abrir uma nova página
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
         
-        # Replace all variables in the value
-        def replace(match):
-            variable_name = match.group(1)
-            # Get the variable value from the dictionary
-            variable_value = self.var_dict.get(variable_name, "")
-            # If the variable value itself contains a nested variable, don't replace it yet
-            if "$" in variable_value:
-                return match.group(0)  # Return the original match
-            return str(variable_value)
+        # Navegar até a página com o formulário
+        await page.goto('URL_DO_SEU_FORMULÁRIO')
 
-        updated_value = self.variable_pattern.sub(replace, original_value)
-        # Update the dictionary with the new value
-        self.var_dict[key] = updated_value
-        return updated_value
+        # Extrair os dados do formulário
+        form_selector = 'SELETOR_DO_SEU_FORMULÁRIO'  # Substitua com o seletor CSS correto
+        df = await extract_form_data(page, form_selector)
+        
+        # Imprimir os dados do formulário
+        print(df)
 
-    def update_variables(self):
-        # Keep updating until there are no more variables to replace
-        while True:
-            replacements_made = False
-            for key, value in self.var_dict.items():
-                if isinstance(value, str) and self.variable_pattern.search(value):
-                    updated_value = self.update_line(key)
-                    if updated_value != value:
-                        replacements_made = True
+        # Fechar o navegador
+        await browser.close()
 
-            if not replacements_made:
-                break
-
-        # Check if there are unresolved variables
-        for key, value in self.var_dict.items():
-            if isinstance(value, str) and self.variable_pattern.search(value):
-                raise ValueError(f"Unresolved variable in key '{key}': '{value}'")
-
-# Example usage
-var_dict = {
-    'var1': 'value1',
-    'var2': 'value2',
-    'var3': '$(var1) and $(var2)',
-    'var4': '$(var3) are combined'
-}
-
-processor = YamlProcessor(var_dict)
-processor.update_variables()
-print(processor.var_dict)
+# Executar o script
+asyncio.run(main())
