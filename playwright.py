279441f10282bbb74d152a2faa8468f8a6de0027
felipepimentel <fpimentel88@gmail.com>
2023-12-12 19:18:04 -0300

    sdsd

--- playwright.py
+++ playwright.py
@@ -1,36 +1,49 @@
 from playwright.async_api import async_playwright
-import asyncio
-import json
 
-async def extract_hierarchy(page, selector):
-    elements = await page.query_selector_all(f'{selector} > *')  # Seleciona todos os elementos filhos diretos da div
-    hierarchy = []
+async def extract_data_labels(page):
+    # Localize todos os elementos com data-type=label
+    data_labels = page.locator('[data-type="label"]')
+    results = []
 
-    for element in elements:
-        tag_name = await element.evaluate('element => element.tagName')
-        label = await element.evaluate("""(element) => {
-            let label = element.querySelector('label');
-            return label ? label.innerText : '';
-        }""")
-        children = await extract_hierarchy(element, '> *')  # Recursivamente obtém a hierarquia dos filhos
-        hierarchy.append({
-            'tag': tag_name,
-            'label': label.strip(),
-            'children': children
+    # Conta o número de elementos encontrados
+    count = await data_labels.count()
+    
+    # Itera sobre os elementos encontrados
+    for i in range(count):
+        # Localize o elemento label dentro do div com data-type=label
+        label = await data_labels.nth(i).locator("label[for]").first()
+        
+        # Captura o texto do label
+        text = await label.inner_text()
+        
+        # Captura o valor do atributo 'for'
+        for_value = await label.get_attribute("for")
+        
+        # Use o valor de 'for' para localizar o elemento correspondente e obter o seu valor
+        element_for = await page.locator(f"#{for_value}").first()
+        element_value = ""
+        if await element_for.count() > 0:  # Verifica se o elemento existe
+            element_value = await element_for.input_value()
+
+        # Adiciona os resultados em uma lista
+        results.append({
+            "for": for_value,
+            "text": text.strip(),
+            "value": element_value
         })
-
-    return hierarchy
+    
+    return results
 
 async def main():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch()
         page = await browser.new_page()
-        await page.goto('http://your-page-url.com')
-        
-        # Suponha que a div que você quer inspecionar tem um id 'my-div'
-        data = await extract_hierarchy(page, '#my-div')
-        print(json.dumps(data, indent=2))
-        
+        await page.goto('https://example.com')  # Substitua pela URL da sua página
+
+        data = await extract_data_labels(page)
+        print(data)  # Imprime o resultado ou faz outra operação desejada
+
         await browser.close()
 
+import asyncio
 asyncio.run(main())
